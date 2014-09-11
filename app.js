24653095b378f9221055fe06ae47aa0d9fb7cbb9
Hafiz Badrie Lubis <hafiz@jakpost.com>
2014-09-11 17:32:07 +0700

    added asynchronous method

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var slasher = require('./slasher'),
 		urlList	= require('./urls.json'),
-		urls 		= urlList['url'];
+		urls 		= urlList['url'],
+		slash 	= new slasher.Slash();
+
+slash.on('slashed', function(content) {
+	console.log(content);
+});
 
 urls.forEach(function(url, index, array) {
-	slasher.slashIt(url);
+	slash.slashIt(url);
 });
--- slasher.js
+++ slasher.js
@@ -1,80 +1,80 @@
 var slasher = (function() {
-	var request = require('request'),
-		cheerio = require('cheerio'),
-		str 		= require('string'),
-		methods;
+	var events = require('events'),
+			request = require('request'),
+			cheerio = require('cheerio'),
+			str 		= require('string'),
+			methods;
 
 	methods = {
-		slashIt: function(url) {
-			request(url, function(error, response, body) {
-				if (!error && response.statusCode == 200) {
-					console.log('URL: ' + url);
+		Slash: function() {
+			events.EventEmitter.call(this);
 
-					var	bodyStripped = str(body).stripTags('blockquote', 'strong', 'em', 'a', 'ul', 'li', 'ol').s,
-							$ 			  	 = cheerio.load(bodyStripped),
-							$htmlBody;
+			this.slashIt = function(url) {
+				var _this = this;
+				request(url, function(error, response, body) {
+					if (!error && response.statusCode == 200) {
+						var	bodyStripped = str(body).stripTags('blockquote', 'strong', 'em', 'a', 'ul', 'li', 'ol').s,
+								$ 			  	 = cheerio.load(bodyStripped),
+								$htmlBody;
 
-					$('script, style, iframe, header, footer, noscript, br, img').remove();
-					$htmlBody = $('body');
+						$('script, style, iframe, header, footer, noscript, br, img').remove();
+						$htmlBody = $('body');
 
-					// console.log($htmlBody.html());
+						var children 	 	= $htmlBody.children(),
+								pChildren  	= $htmlBody.children('p'),
+								pChildCount = pChildren.length,
+								childCount 	= children.length,
+								done 			 	= false,
+								foundText  	= '';
 
-					var children 	 	= $htmlBody.children(),
-							pChildren  	= $htmlBody.children('p'),
-							pChildCount = pChildren.length,
-							childCount 	= children.length,
-							done 			 	= false,
-							foundText  	= '';
+						while(!done) {
+							var topIdx   	 = 0,
+									textLength = 0;
 
-					while(!done) {
-						var topIdx   	 = 0,
-								textLength = 0;
+							if (pChildCount > 0) {
+								for(var i=0; i<pChildCount; i++) {
+									var pChild = $(pChildren[i]);
+									foundText += pChild.text() + '\n';
+								}
 
-						if (pChildCount > 0) {
-							for(var i=0; i<pChildCount; i++) {
-								var pChild = $(pChildren[i]);
-								foundText += pChild.text() + '\n';
-							}
+								done = true;
+							} else {
+								for(var i=0; i<childCount; i++) {
+									var content = $(children[i]),
+											text 	 	= content.text().replace(/\s/g, '');
 
-							done = true;
-						} else {
-							for(var i=0; i<childCount; i++) {
-								var content = $(children[i]),
-										text 	 	= content.text().replace(/\s/g, '');
+									if (textLength < text.length) {
+										textLength = text.length;
+										topIdx = i;
+									}
+								}
 
-								if (textLength < text.length) {
-									textLength = text.length;
-									topIdx = i;
+								var child = $(children[topIdx]);
+
+								children 		= child.children();
+								pChildren 	= child.children('p');
+								pChildCount = pChildren.length;
+								childCount 	= child.children().length;
+								if (childCount === 0) {
+									foundText = child.text();
+									done = true;
 								}
 							}
-
-							var child = $(children[topIdx]);
-
-							children 		= child.children();
-							pChildren 	= child.children('p');
-							pChildCount = pChildren.length;
-							childCount 	= child.children().length;
-							if (childCount === 0) {
-								foundText = child.text();
-								done = true;
-							}
 						}
+
+						_this.emit('slashed', foundText);
+						//console.log(foundText);
+					} else {
+						_this.emit('requestError');
 					}
-
-					console.log(foundText);
-
-					console.log('============================================================');
-					console.log('');
-
-				} else {
-					return false;
-				}
-			});
+				});
+			}
 		}
-	};
+	}
+
+	methods.Slash.prototype = Object.create(events.EventEmitter.prototype);
 
 	return methods;
-
 })();
 
 module.exports = slasher;
