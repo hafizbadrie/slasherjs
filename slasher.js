9f0b0cab59fb1c4ffb711a5ee53fa1f9372a31d2
Hafiz Badrie Lubis <hafiz@jakpost.com>
2014-09-15 13:11:07 +0700

    improve the extraction algorithm. now it uses dfs methodology

--- slasher.js
+++ slasher.js
@@ -8,58 +8,74 @@ var slasher = (function() {
 	methods = {
 		Slash: function() {
 			events.EventEmitter.call(this);
+			var contentLib = {},
+					countArr 	 = [];
+
+			this.recurslash = function($, children) {
+				for(var i=0; i<children.length; i++) {
+					var $child = $(children[i]);
+
+					// console.log($child.text());
+					// console.log('===============================');
+
+					if ($child.children('p').length > 0) {
+						var pChildren = $child.children('p'),
+								pContent 	= '',
+								pTrimmed  = '';
+						for(var j=0; j<pChildren.length; j++) {
+							pContent += $(pChildren[j]).text() + '\n';
+						}
+
+						pTrimmed = pContent.replace(/\s/g, '');
+						countArr.push(pTrimmed.length);
+						contentLib[pTrimmed.length] = pContent;
+					} 
+
+					if ($child.children().length > 0) {
+						this.recurslash($, $child.children());
+					} else {
+						if ($child.text() !== '') {
+							var content = $child.text(),
+									trimmed = content.replace(/\s/g, '');
+							countArr.push(trimmed.length);
+							contentLib[trimmed.length] = content;
+						}
+					}
+				}
+			}
+
 			this.slasher = function(htmlCode, callback) {
 				var startTime = new Date().getMilliseconds();
 
+				contentLib = {};
+				countArr 	 = [];
+
 				var	bodyStripped = str(htmlCode).stripTags('blockquote', 'strong', 'em', 'a', 'ul', 'li', 'ol').s,
 						$ 			  	 = cheerio.load(bodyStripped),
+						_this 			 = this,
 						$htmlBody;
 
 				process.nextTick(function() {
 					try {
+						var foundText = '';
+
 						$('script, style, iframe, header, footer, noscript, br, img').remove();
 						$htmlBody = $('body');
 
 						var children 	 	= $htmlBody.children(),
-								pChildren  	= $htmlBody.children('p'),
-								pChildCount = pChildren.length,
 								childCount 	= children.length,
 								done 			 	= false,
 								foundText  	= '';
 
-						while(!done) {
-							var topIdx   	 = 0,
-									textLength = 0;
+						_this.recurslash($, children);
 
-							if (pChildCount > 0) {
-								for(var i=0; i<pChildCount; i++) {
-									var pChild = $(pChildren[i]);
-									foundText += pChild.text() + '\n';
-								}
+						// console.log(contentLib);
 
-								done = true;
-							} else {
-								for(var i=0; i<childCount; i++) {
-									var content = $(children[i]),
-											text 	 	= content.text().replace(/\s/g, '');
-
-									if (textLength < text.length) {
-										textLength = text.length;
-										topIdx = i;
-									}
-								}
-
-								var child = $(children[topIdx]);
-
-								children 		= child.children();
-								pChildren 	= child.children('p');
-								pChildCount = pChildren.length;
-								childCount 	= child.children().length;
-								if (childCount === 0) {
-									foundText = child.text();
-									done = true;
-								}
-							}
+						if (countArr.length > 0) {
+							countArr.sort(function(a, b) {
+								return b - a;
+							});
+							foundText = contentLib[countArr[0]];
 						}
 
 						var endTime  = new Date().getMilliseconds(),
@@ -69,7 +85,7 @@ var slasher = (function() {
 
 						callback('ok', foundText);
 					} catch(ex) {
-						callback('error', '');
+						callback('error', ex);
 					}
 				});
 
@@ -83,7 +99,7 @@ var slasher = (function() {
 							if (status === 'ok') {
 								_this.emit('slashed', foundText);
 							} else {
-								_this.emit('error', 'Fail in slashing HTML code');
+								_this.emit('error', foundText);
 							}
 						});
 					} else {
